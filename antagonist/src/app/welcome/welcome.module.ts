e9611c240dd62129822e22d906efd84e2f4a2d7e
kamil <kamil.galek>
2019-08-18 13:33:15 +0200

    app initializer after implementation

--- antagonist/src/app/welcome/welcome.module.ts
+++ antagonist/src/app/welcome/welcome.module.ts
@@ -4,5 +4,21 @@ import {AbstractConsoleService} from "../console/abstract-console.service";
 
 @NgModule({
   declarations: [],
+  providers: [
+    {
+      multi: true,
+      provide: APP_INITIALIZER,
+      useFactory: (consoleService: AbstractConsoleService) => {
+        return () => {
+          consoleService.info(`HI! Aren't you awesome web developer?` +
+            `We have job for you! Check it out and apply for it at hire_me@awesome.xyz`);
+        };
+      },
+      deps: [AbstractConsoleService]
+    }
+  ],
+  imports: [
+    ConsoleModule
+  ]
 })
 export class WelcomeModule { }
